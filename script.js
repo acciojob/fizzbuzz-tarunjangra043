7d652672bdba8b3b8fc89ebb034b5354d2be0f33
Tarun Jangra <111531292+tarunjangra043@users.noreply.github.com>
2024-08-07 21:02:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+
+for (let i = 1; i <= 20; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        alert("FizzBuzz");
+    } 
+    else if (i % 3 === 0) {
+        alert("Fizz");
+    } 
+    else if (i % 5 === 0) {
+        alert("Buzz");
+    }
+    else {
+        alert(i.toString());
+    }
+}
